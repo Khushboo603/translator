e0e65e4aaa9d0bf295089e6ee856190d8f4d8329
Khushboo Kumari <khushboo.kumari@nusummit.com>
2026-03-17 16:14:29 +0530

    issue resolve for post home api

--- translator/main/views.py
+++ translator/main/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# from translate import Translator
 from translate import Translator
 
 def home(request):
     if request.method == "POST":
-        text = request.POST["translate"]
-        language = request.POST["language"]
+        # text = request.POST["translate"]
+        # language = request.POST["language"]
+
+        text = request.POST.get("translate", "").strip()
+        language = request.POST.get("language", "").strip()
+
+
         translator = Translator(to_lang=language)
         translation = translator.translate(text)
         return HttpResponse(translation)

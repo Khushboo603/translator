c5f1606efbf6a33ac8916eb3d1deb005c36061c8
Khushboo Kumari <khushboo.kumari@nusummit.com>
2026-03-17 12:31:18 +0530

    Translator api done

---
+++ translator/main/urls.py
@@ -0,0 +1,21 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.home, name="home"),
+# ]
+
+
+# from django.urls import path
+# from .views import *
+
+# urlpatterns = [
+#     path('',home,name="home"),
+# ]
+
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',home,name="home"),
+]
--- translator/main/views.py
+++ translator/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from translate import Translator
+
+def home(request):
+    if request.method == "POST":
+        text = request.POST["translate"]
+        language = request.POST["language"]
+        translator = Translator(to_lang=language)
+        translation = translator.translate(text)
+        return HttpResponse(translation)
+    return render(request, "main/index.html")

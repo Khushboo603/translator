623491ed4711ce868511edeebf29f4424b53d547
Khushboo Kumari <khushboo.kumari@nusummit.com>
2026-04-06 15:23:00 +0530

    Added error handling functionality in newslatteremail

--- translator/main/views.py
+++ translator/main/views.py
@@ -44,14 +44,49 @@ def home(request):
             except:
                 return HttpResponse("Wrong Input")
             return render(request,"main/index.html",{"result":result})
+        # elif 'subscribe' in request.POST:
+        #     email = newslatteremail()
+        #     email.userEmail = request.POST.get("email")
+        #     email.save()
+        #     messages.info(request, 'You have successfully subscribed to your newslatter.')
+        # elif 'unsubscribe' in request.POST:
+        #     newslatteremail.objects.get(
+        #         userEmail = request.POST.get("email")
+        #     ).delete()
+        #     messages.info(request, 'Sorry to see you!!!')
         elif 'subscribe' in request.POST:
-            email = newslatteremail()
-            email.userEmail = request.POST.get("email")
-            email.save()
-            messages.info(request, 'You have successfully subscribed to your newslatter.')
+            # email = newslatteremail()
+            try:
+                user_email = request.POST.get("email")
+
+                if not user_email:
+                    raise ValeError("Email is required")
+
+                if newslatteremail.objects.filter(userEmail=user_email).exists():
+                    messages.warning(request, "You are already subscribed!")
+                else:
+                    email = newslatteremail(userEmail=user_email)
+                    email.save()
+                    messages.success(request, 'You have successfully subscribed to your newslatter.')
+            except Exception as e:
+                messages.error(request, f"Something went wrong: {str(e)}")
+
+
         elif 'unsubscribe' in request.POST:
-            newslatteremail.objects.get(
-                userEmail = request.POST.get("email")
-            ).delete()
-            messages.info(request, 'Sorry to see you!!!')
+            try:
+                user_email = request.POST.get("email")
+
+                if not user_email:
+                    raise ValeError("Email is required")
+
+
+                obj = newslatteremail.objects.get(userEmail = user_email)
+                obj.delete()
+                messages.success(request, 'Sorry to see you!!!')
+            
+            except newslatteremail.DoesNotExist:
+                messages.error(request, "Email not found!")
+
+            except Exception as e:
+                messages.error(request, f"Something went wrong: {str(e)}")
     return render(request, "main/index.html")
